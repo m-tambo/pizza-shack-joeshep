4e7dc812c9cfcb14d53a45de7560d30c5cd77d97
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-30 12:52:10 -0500

    fresh routes

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const { Router } = require('express')
+const router = Router()
+
+// ______public routes______
+router.use(require('./about'))
+// router.use(require('./contact'))
+// router.use(require('./login'))
+// router.use(require('./register'))
+router.use(require('./root'))
+
+// TODO: define isAuthenticated
+router.use( (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+})
+
+// ______private routes______
+// router.use(require('./logout'))
+// router.use(require('./order'))
+
+module.exports = router
