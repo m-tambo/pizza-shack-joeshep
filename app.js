da9395a124877cdf2084858676c1fe774d1e645b
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-30 15:09:18 -0500

    rerouting the login page

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ app.locals.body.magic = "wooooahhh, it's magic!"
 app.use(express.static('public'))
 app.use(routes)
 
-app.get('/login', (req, res, next) => {
-  res.render('login', {page: 'Login'})
-})
-
 app.get('/register', (req, res, next) => {
   res.render('register', {page: 'Register'})
 })
