7aa0341f9635ffdeddeddc826fb9878ecd6860c3
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-30 12:53:27 -0500

    rerouting the index

--- app.js
+++ app.js
@@ -2,16 +2,36 @@
 
 const express = require('express')
 const app = express()
+const routes = require('./routes/')  // defaults to looking for 'index' file
 
 // _______pug config_______
 app.set('view engine', 'pug')
 
-// _______middleware_______
-app.use(express.static('public'))
+// ______global variables______
+app.locals.company = "🍕 Joe's Pizza Shack"
+app.locals.body = {}
+app.locals.body.magic = "wooooahhh, it's magic!"
 
-app.get('/', (req, res, next) => {
-  res.render('index')
+// _______middlewares_______
+app.use(express.static('public'))
+app.use(routes)
+
+app.get('/contact', (req, res, next) => {
+  res.render('contact', {page: 'Contact'})
 })
+
+app.get('/login', (req, res, next) => {
+  res.render('login', {page: 'Login'})
+})
+
+app.get('/register', (req, res, next) => {
+  res.render('register', {page: 'Register'})
+})
+
+app.use((req, res) => {
+  res.render('404')
+})
+//______end of middlewares______
 
 
 const port = process.env.PORT || 3000
